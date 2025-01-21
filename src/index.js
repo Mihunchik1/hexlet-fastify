2b5f640fb10a4719229b35195bf8306129ac74f4
Mihunchik <arinushkin048@mail.ru>
2025-01-21 17:33:24 +0300

    upd index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import fastify from 'fastify';
 const app = fastify();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/users', (req, res) => {
+  res.send('GET /users');
+});
+
+app.post('/users', (req, res) => {
+  res.send('POST /users');
 });
 
 app.listen({ port }, () => {
